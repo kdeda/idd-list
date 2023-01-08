94e50a96ea2fb699d4387c55399f3873c221bd81
Klajd Deda <kdeda@id-design.com>
2023-01-07 19:48:13 -0500

    WIP

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/kdeda/idd-log4-swift.git", from: "1.2.1")
+        .package(url: "https://github.com/kdeda/idd-log4-swift.git", from: "1.2.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
